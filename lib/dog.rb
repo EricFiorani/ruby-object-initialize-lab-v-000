e8b1f6a761a08d2abddb2290e51385d1b049ded9
Eric Fiorani <efiorani305@gmail.com>
2018-03-21 14:21:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,5 @@
 class Dog
-  def initialize(name)
-    @name = name
-  end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
+  def initialize(name, breed = "Mutt")
+    @name= name
+    @breed= breed
 end
